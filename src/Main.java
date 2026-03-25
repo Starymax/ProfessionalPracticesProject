e10e865bbb024fda3491ff76ee8471cddb19a1b5
Diego <leondiego453@gmail.com>
2026-03-25 01:26:34 -0600

    Connection with database created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+import mx.fei.dataaccess.DatabaseConnectionManager;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+    public static void main(String[] args) {
+        DatabaseConnectionManager dbManager;
+        try {
+            dbManager = DatabaseConnectionManager.buildConnection();
+            dbManager.close();
+        } catch (SQLException e) {
+            LOG.log(Level.SEVERE, "Error al conectar con la base de datos", e);
+        }
+    }
 }
--- src/mx/fei/dataaccess/DatabaseConnectionManager.java
+++ src/mx/fei/dataaccess/DatabaseConnectionManager.java
@@ -1,5 +1,59 @@
 package mx.fei.dataaccess;
 
-public class DatabaseConnectionManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class DatabaseConnectionManager {
+    private static final Logger logger = Logger.getLogger(DatabaseConnectionManager.class.getName());
+    private  static DatabaseConnectionManager connSingleton;
+    private Connection connection;
+    private String driver;
+    private String url;
+    private String host = "localhost";
+    private String port = "3306";
+    private String dataBase = "practicas_profesionales";
+    private String username = "administradorGeneral";
+    private String password = "Admin12345";
+
+    private DatabaseConnectionManager() throws SQLException {
+        driver = "com.mysql.cj.jdbc.Driver";
+        url = "jdbc:mysql://" + host + ":" + port + "/" + dataBase + "?useTimezone=true&serverTimezone=UTC";
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            logger.log(Level.SEVERE,"No se encontro el driver JDBC" + driver,e);
+        }
+        connect();
+    }
+
+    private void connect() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(url,username,password);
+            logger.log(Level.INFO,"Conexion establecida con la base de datos");
+        }
+    }
+
+    public void close() throws SQLException {
+        if (connection != null) {
+            connection.close();
+            logger.log(Level.INFO,"Conexion cerrada");
+        }
+    }
+
+    public PreparedStatement preparedStatement(String query) throws SQLException {
+        connect();
+        return connection.prepareStatement(query);
+    }
+
+    public static DatabaseConnectionManager buildConnection() throws SQLException {
+        if (connSingleton == null) {
+            connSingleton = new DatabaseConnectionManager();
+        }
+        connSingleton.connect();
+        return connSingleton;
+    }
 }
